bfd606438de025fee0548fb23d542b46d37e8a78
Vladimir Petrakovich <v.petrakovich@skbkontur.ru>
2019-08-28 15:09:04 +0700

    remove deprecated extensions for Logger.Companion
    Removed the following extensions:
     - Logger.Companion.forClass()
     - Logger.Companion.currentClass()
    
    Member functions of Logger.Companion should be used instead.
    
    The extensions were deprecated for some time.
    Since they are inline, removing of them does not affect ABI compatibility.

--- src/main/kotlin/ru/kontur/jinfra/logging/DefaultLoggerFactory.kt
+++ src/main/kotlin/ru/kontur/jinfra/logging/DefaultLoggerFactory.kt
@@ -3,7 +3,6 @@ package ru.kontur.jinfra.logging
 import ru.kontur.jinfra.logging.backend.slf4j.Slf4jBackend
 import ru.kontur.jinfra.logging.decor.MessageDecor
 import ru.kontur.jinfra.logging.decor.PrefixMessageDecor
-import kotlin.reflect.KClass
 
 /**
  * Default implementation of [LoggerFactory].
@@ -16,24 +15,4 @@ object DefaultLoggerFactory : LoggerFactory.Wrapper() {
 
     override fun getEmptyDecor(): MessageDecor = PrefixMessageDecor.EMPTY
 
-}
-
-// todo: remove before release
-@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
-@Deprecated(
-    "use member method",
-    replaceWith = ReplaceWith("Logger.forClass(kClass)", imports = ["ru.kontur.jinfra.logging.Logger"])
-)
-fun Logger.Companion.forClass(kClass: KClass<*>): Logger {
-    return DefaultLoggerFactory.getLogger(kClass)
-}
-
-// todo: remove before release
-@Suppress("NOTHING_TO_INLINE", "EXTENSION_SHADOWED_BY_MEMBER")
-@Deprecated(
-    "use member method",
-    replaceWith = ReplaceWith("Logger.currentClass()", imports = ["ru.kontur.jinfra.logging.Logger"])
-)
-inline fun Logger.Companion.currentClass(): Logger {
-    return DefaultLoggerFactory.currentClassLogger()
 }
