c79839ea2688a7d32c352b6f59720855f0de609d
Vladimir Petrakovich <v.petrakovich@skbkontur.ru>
2021-12-29 20:04:42 +0300

    1.1: add creation instant to slf4j marker

--- src/main/kotlin/ru/kontur/kinfra/logging/backend/slf4j/LoggingRequestMarker.kt
+++ src/main/kotlin/ru/kontur/kinfra/logging/backend/slf4j/LoggingRequestMarker.kt
@@ -3,6 +3,7 @@ package ru.kontur.kinfra.logging.backend.slf4j
 import org.slf4j.Marker
 import org.slf4j.MarkerFactory
 import ru.kontur.kinfra.logging.backend.LoggingRequest
+import java.time.Instant
 
 /**
  * Marker that allows an SLF4J implementation to access [LoggingRequest].
@@ -13,6 +14,13 @@ public class LoggingRequestMarker private constructor(
     public val request: LoggingRequest,
     private val delegate: Marker
 ) : Marker by delegate {
+
+    /**
+     * Time of creation of this marker.
+     *
+     * Provides higher resolution than `LoggingEvent.getTimeStamp()` from Logback 1.2.
+     */
+    public val createdAt: Instant = Instant.now()
 
     public constructor(request: LoggingRequest) : this(request, MarkerFactory.getDetachedMarker(NAME))
 
